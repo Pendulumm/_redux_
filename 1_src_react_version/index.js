6bff1c22662acca715fde7691389a70deb8ceb70
Jrm <Jrm@git.com>
2023-08-11 17:51:55 +0800

    redux_mini

--- 1_src_react_version/index.js
+++ 1_src_react_version/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
 );
-
-store.subscribe(() => {
-  // DOM diffing不会引起大面积重绘重排
-  root.render(
-    <App />
-  );
-})
 
